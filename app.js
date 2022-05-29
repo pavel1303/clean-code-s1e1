9fa59a39e54d43db93b09963ada73d8ab7885715
Pavel Lebedev <lebedpavel.dev@gmail.com>
2022-05-29 17:34:21 +0300

    refactor: change js code to restore functionality

--- app.js
+++ app.js
@@ -8,17 +8,17 @@
 
 // Event handling, user interaction is what starts the code execution.
 
-var taskInput=document.getElementById("new-task");//Add a new task.
-var addButton=document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder=document.getElementById("incompleteTasks");//ul of #incompleteTasks
-var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
+var taskInput=document.querySelector(".add-section__input");//Add a new task.
+var addButton=document.querySelector(".add-section__btn");//first button
+var incompleteTaskHolder=document.querySelector(".todo-section__list");//ul of #incompleteTasks
+var completedTasksHolder=document.querySelector(".completed-section__list");//completed-tasks
 
 
 //New task list item
 var createNewTaskElement=function(taskString){
 
     var listItem=document.createElement("li");
-
+    listItem.className="app-section__item";
     //input (checkbox)
     var checkBox=document.createElement("input");//checkbx
     //label
@@ -33,17 +33,19 @@ var createNewTaskElement=function(taskString){
     var deleteButtonImg=document.createElement("img");//delete button image
 
     label.innerText=taskString;
-    label.className='task';
+    label.className="app-section__label";
 
     //Each elements, needs appending
     checkBox.type="checkbox";
+    checkBox.className="app-section__checkbox";
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="app-section__input";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className="app-section__btn btn";
 
-    deleteButton.className="delete";
+    deleteButton.className="btn-delete";
+    deleteButtonImg.className='btn-delete__img';
     deleteButtonImg.src='./remove.svg';
     deleteButton.appendChild(deleteButtonImg);
 
@@ -82,10 +84,10 @@ var editTask=function(){
 
     var listItem=this.parentNode;
 
-    var editInput=listItem.querySelector('input[type=text]');
-    var label=listItem.querySelector("label");
-    var editBtn=listItem.querySelector(".edit");
-    var containsClass=listItem.classList.contains("editMode");
+    var editInput=listItem.querySelector('.app-section__input');
+    var label=listItem.querySelector(".app-section__label");
+    var editBtn=listItem.querySelector(".app-section__btn");
+    var containsClass=listItem.classList.contains("app-section__item--edit-mode");
     //If class of the parent is .editmode
     if(containsClass){
 
@@ -99,7 +101,7 @@ var editTask=function(){
     }
 
     //toggle .editmode on the parent.
-    listItem.classList.toggle("editMode");
+    listItem.classList.toggle("app-section__item--edit-mode");
 };
 
 
@@ -155,9 +157,9 @@ addButton.addEventListener("click",ajaxRequest);
 var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
     console.log("bind list item events");
 //select ListItems children
-    var checkBox=taskListItem.querySelector("input[type=checkbox]");
-    var editButton=taskListItem.querySelector("button.edit");
-    var deleteButton=taskListItem.querySelector("button.delete");
+    var checkBox=taskListItem.querySelector(".app-section__checkbox");
+    var editButton=taskListItem.querySelector(".app-section__btn");
+    var deleteButton=taskListItem.querySelector(".btn-delete");
 
 
     //Bind editTask to edit button.
