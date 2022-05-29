ff04e4e08da97c99aa54efe8f18ba7a29d685db9
Pavel Lebedev <lebedpavel.dev@gmail.com>
2022-05-29 18:13:17 +0300

    refactor: add atribute-"alt" for img in HTML- paragraph 1.2 advanced level and add atribute-"alt" for new components in js

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ var createNewTaskElement=function(taskString){
     deleteButton.className="btn-delete";
     deleteButtonImg.className='btn-delete__img';
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.alt='Delete button';
     deleteButton.appendChild(deleteButtonImg);
 
 
